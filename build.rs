925591ebf12616abe8c9882ddeb68a52800c7775
Taras Palczynski <taras.palczynski@deepgram.com>
2026-03-11 11:34:53 -0400

    Move from log to defmt

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() {
     load_dotenv();
     linker_be_nice();
+    println!("cargo:rustc-link-arg=-Tdefmt.x");
     // make sure linkall.x is the last linker script (otherwise might cause problems with flip-link)
     println!("cargo:rustc-link-arg=-Tlinkall.x");
 }
